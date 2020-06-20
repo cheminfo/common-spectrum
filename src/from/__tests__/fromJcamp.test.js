35e6e3ff086eb93725f0540a984c082ac21661bf
Luc Patiny <luc@patiny.com>
2020-06-20 10:22:33 +0200

    test: add ntuples jcamp example

--- src/from/__tests__/fromJcamp.test.js
+++ src/from/__tests__/fromJcamp.test.js
@@ -3,29 +3,44 @@ import { join } from 'path';
 
 import { fromJcamp } from '../fromJcamp';
 
-test('fromJcamp', () => {
-  let jcamp = readFileSync(
-    join(__dirname, '../../../testFiles/jcamp.jdx'),
-    'utf8',
-  );
+describe('fromJcamp', () => {
+  it('normal', () => {
+    let jcamp = readFileSync(
+      join(__dirname, '../../../testFiles/jcamp.jdx'),
+      'utf8',
+    );
 
-  let result = fromJcamp(jcamp);
+    let result = fromJcamp(jcamp);
 
-  expect(result.spectra).toHaveLength(2);
+    expect(result.spectra).toHaveLength(2);
 
-  let first = result.spectra[0];
+    let first = result.spectra[0];
 
-  expect(first.x).toHaveLength(2251);
-  expect(first.y).toHaveLength(2251);
-  expect(first.xLabel).toStrictEqual('Ts [°C]');
-  expect(first.yLabel).toStrictEqual('Value [mg]');
-  expect(first.flavor).toStrictEqual('mg vs °C');
+    expect(first.x).toHaveLength(2251);
+    expect(first.y).toHaveLength(2251);
+    expect(first.xLabel).toStrictEqual('Ts [°C]');
+    expect(first.yLabel).toStrictEqual('Value [mg]');
+    expect(first.flavor).toStrictEqual('mg vs °C');
 
-  let second = result.spectra[1];
+    let second = result.spectra[1];
 
-  expect(second.x).toHaveLength(2251);
-  expect(second.y).toHaveLength(2251);
-  expect(second.xLabel).toStrictEqual('t [s]');
-  expect(second.yLabel).toStrictEqual('Value [mg]');
-  expect(second.flavor).toStrictEqual('mg vs s');
+    expect(second.x).toHaveLength(2251);
+    expect(second.y).toHaveLength(2251);
+    expect(second.xLabel).toStrictEqual('t [s]');
+    expect(second.yLabel).toStrictEqual('Value [mg]');
+    expect(second.flavor).toStrictEqual('mg vs s');
+  });
+
+  it('ntuples', () => {
+    let jcamp = readFileSync(
+      join(__dirname, '../../../testFiles/ntuples.jdx'),
+      'utf8',
+    );
+
+    let result = fromJcamp(jcamp).spectra[0];
+    expect(result.x).toHaveLength(408);
+    expect(result.y).toHaveLength(408);
+    expect(result.xUnits).toBe('µm');
+    expect(result.yUnits).toBe('µg');
+  });
 });
