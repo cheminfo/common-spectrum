778e674db1b267bba951d3f74ca5ae44c2179b46
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-12-11 10:44:07 +0100

    feat: add function for multiple spectra retrieval

---
+++ src/util/__tests__/getMultiple.test.js
@@ -0,0 +1,51 @@
+import { Analysis } from '../..';
+import { getMultiple } from '../getMultiple';
+
+const spectra = {
+  variables: {
+    x: {
+      data: [1, 2],
+      units: 'V',
+      label: 'Voltage',
+    },
+    y: {
+      data: [0.5, 0.2],
+      units: 'A',
+      label: 'Current',
+    },
+    z: {
+      data: [5, 6],
+      units: '°C',
+      label: 'Temperature',
+    },
+    t: {
+      data: [7, 8],
+      units: 's',
+      label: 'Time',
+    },
+  },
+};
+
+test('simple test case', () => {
+  const len = 3;
+  let analyses = new Array(len);
+  for (let i = 0; i < len; i++) {
+    analyses[i] = new Analysis();
+    analyses[i].pushSpectrum(spectra.variables, { title: `Vg = ${i + 3}` });
+  }
+
+  // ignored value
+  analyses.push(new Analysis());
+
+  const result = getMultiple(analyses, {
+    xLabel: 'Voltage',
+    xUnits: 'V',
+    yLabel: 'Current',
+    yUnits: 'A',
+  });
+  expect(result.series).toHaveLength(len);
+  expect(result.axes).toStrictEqual({
+    x: { label: 'Voltage [V]' },
+    y: { label: 'Current [A]' },
+  });
+});
---
+++ src/util/getMultiple.js
@@ -0,0 +1,37 @@
+/**
+ * Generate a jsgraph chart format from an array of Analysis
+ * @param {Array<Analysis>} analyses
+ * @param {object} selector
+ */
+export function getMultiple(analyses, selector) {
+  let series = [];
+
+  let xLabel = '';
+  let yLabel = '';
+  let xUnits = '';
+  let yUnits = '';
+
+  for (let i = 0; i < analyses.length; i++) {
+    const analysis = analyses[i];
+    const spectra = analysis.getXYSpectrum(selector);
+    if (!spectra) continue;
+    if (!xLabel) xLabel = spectra.variables.x.label;
+    if (!yLabel) yLabel = spectra.variables.y.label;
+    if (!xUnits) xUnits = spectra.variables.x.units;
+    if (!yUnits) yUnits = spectra.variables.y.units;
+
+    const serie = {
+      x: spectra.variables.x.data,
+      y: spectra.variables.y.data,
+      label: spectra.title,
+    };
+    series.push(serie);
+  }
+  return {
+    axes: {
+      x: { label: `${xLabel} [${xUnits}]` },
+      y: { label: `${yLabel} [${yUnits}]` },
+    },
+    series,
+  };
+}
