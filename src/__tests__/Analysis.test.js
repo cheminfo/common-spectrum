5dd684eadd6cca76f96d4d4e984c359a51ef4eeb
Luc Patiny <luc@patiny.com>
2021-02-17 09:39:03 +0100

    chore: add getXY testcase

---
+++ src/__tests__/Analysis.test.js
@@ -0,0 +1,22 @@
+
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+import { fromJcamp } from '..';
+
+describe('Analysis', () => {
+
+    it('getXY', () => {
+        const text = readFileSync(
+            join(__dirname, '../../testFiles/xps.jdx'),
+            'utf-8',
+        );
+
+        let analysis = fromJcamp(text);
+
+        let xy = analysis.getXY();
+        expect(xy.x).toHaveLength(91)
+        expect(xy.y).toHaveLength(91)
+    })
+});
+
