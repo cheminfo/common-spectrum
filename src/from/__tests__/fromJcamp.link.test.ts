21035e4718efc463641781673ce1864665a735fb
Luc Patiny <luc@patiny.com>
2021-07-20 14:46:59 +0200

    chore: fix testcase

--- src/from/__tests__/fromJcamp.link.test.ts
+++ src/from/__tests__/fromJcamp.link.test.ts
@@ -7,21 +7,20 @@ test('fromJcamp linked file', () => {
   let jcamp = readFileSync(join(__dirname, './data/uv-link.jdx'), 'utf8');
 
   let result = fromJcamp(jcamp);
-  console.log(result);
 
   expect(result.spectra).toHaveLength(2);
 
   let first = result.spectra[0];
 
-  expect(first.variables.x.data).toHaveLength(2251);
-  expect(first.variables.y.data).toHaveLength(2251);
-  expect(first.variables.x.label).toStrictEqual('Ts [°C]');
-  expect(first.variables.y.label).toStrictEqual('Value [mg]');
+  expect(first.variables.x.data).toHaveLength(911);
+  expect(first.variables.y.data).toHaveLength(911);
+  expect(first.variables.x.label).toStrictEqual('NANOMETERS');
+  expect(first.variables.y.label).toStrictEqual('ABSORBANCE');
 
   let second = result.spectra[1];
 
-  expect(second.variables.x.data).toHaveLength(2251);
-  expect(second.variables.y.data).toHaveLength(2251);
-  expect(second.variables.x.label).toStrictEqual('t [s]');
-  expect(second.variables.y.label).toStrictEqual('Value [mg]');
+  expect(second.variables.x.data).toHaveLength(911);
+  expect(second.variables.y.data).toHaveLength(911);
+  expect(second.variables.x.label).toStrictEqual('NANOMETERS');
+  expect(second.variables.y.label).toStrictEqual('VARIANCE');
 });
