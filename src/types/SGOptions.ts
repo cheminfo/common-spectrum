aa41e11be88b15326adcaa8a96a52cd5d5850049
Luc Patiny <luc@patiny.com>
2022-06-22 08:30:51 +0200

    chore: remove types to use original package types

--- src/types/SGOptions.ts
+++
@@ -1,6 +0,0 @@
-export interface SGOptions {
-  /** Points to use in the approximations (default: 9) */
-  windowSize: number;
-  /** Degree of the polynomial to use in the approximations (default: 3) */
-  polynomial: number;
-}
--- src/types/ShapeOptions.ts
+++
@@ -1,6 +0,0 @@
-export interface ShapeOptions {
-  /** kind of shape; lorentzian, gaussian and pseudovoigt are supported. */
-  kind: string;
-  /** options depending the kind of shape */
-  options?: any;
-}
