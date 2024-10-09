00c6c3e3a7fa90ff3213047c62f9d1fa71452935
Luc Patiny <luc@patiny.com>
2024-10-09 13:16:02 +0200

    chore: enforce filter must have same size

--- src/to/__tests__/toText.test.ts
+++ src/to/__tests__/toText.test.ts
@@ -6,14 +6,14 @@ import { toText } from '../toText';
 const analysis = new Analysis();
 analysis.pushSpectrum({
   x: {
-    data: [1, 2],
+    data: [1, 2, 3],
     min: 1,
     max: 2,
     units: 'V',
     label: 'voltage',
   },
   y: {
-    data: [3, 4],
+    data: [3, 4, 5],
     min: 3,
     max: 4,
     units: 'mA',
@@ -32,7 +32,7 @@ describe('toCvs', () => {
   it('all variables', () => {
     const result = toText(analysis);
     expect(result).toHaveLength(1);
-    expect(result[0]).toBe('voltage,current,seconds\n1,3,5\n2,4,6\n,,7');
+    expect(result[0]).toBe('voltage,current,seconds\n1,3,5\n2,4,6\n3,5,7');
   });
 
   it('select variables', () => {
@@ -40,7 +40,7 @@ describe('toCvs', () => {
       selector: { xLabel: 'seconds', yLabel: 'voltage' },
     });
     expect(result).toHaveLength(1);
-    expect(result[0]).toBe('seconds,voltage\n5,1\n6,2\n7,');
+    expect(result[0]).toBe('seconds,voltage\n5,1\n6,2\n7,3');
   });
 
   it('empty analysis', () => {
